5e6ef86cacf137e3f7b4e637a49252ed9c7fcaf7
Florian Morina <florianmorina726@gmail.com>
2022-12-23 09:19:08 +0100

    SigIn

--- main.js
+++ main.js
@@ -7,3 +7,18 @@ window.addEventListener("scroll", () => {
         nav.classList.remove("scrolled");
     }
 })
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
